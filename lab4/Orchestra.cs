1282152d0063d373433d8d7f4d6a12ec2462237e
SashaBondarchuk <o.bondarchuk9962@gmail.com>
2022-05-05 12:37:11 +0300

    test commit

---
+++ lab4/Orchestra.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    internal class Orchestra
+    {
+        private List<MusicalInstrument> musicalInstrumentList = new List<MusicalInstrument>();
+
+        public Orchestra()
+        {
+            musicalInstrumentList.Add(new PercussionInstrument(name: "Барабан"));
+            musicalInstrumentList.Add(new PercussionInstrument(name: "Великий барабан"));
+            musicalInstrumentList.Add(new StringInstrument(name: "Гітара", isTuned: false, stringCount: 6));
+            musicalInstrumentList.Add(new WindInstrument(name: "Сопілка"/*, isTuned: true*/));
+            musicalInstrumentList.Add(new WindInstrument(name: "Саксофон", isTuned: true));
+        }
+        public void Show()
+        {
+            foreach (var musicalinstrument in musicalInstrumentList)
+            {
+                musicalinstrument.ShowInfo();
+            }   
+        }
+
+        public void Sounds()
+        {
+            foreach (var musicalinstrument in musicalInstrumentList)
+            {
+                musicalinstrument.MakeSound();
+            }
+        }
+    }
+}
